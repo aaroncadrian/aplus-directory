332905c0d26d881bc7ab69d19cb490d7852360f4
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-27 18:36:02 -0700

    Add action buttons

--- libs/web-directory/people/feature-create/src/lib/create-person-page.tsx
+++ libs/web-directory/people/feature-create/src/lib/create-person-page.tsx
@@ -1,15 +1,36 @@
-import { Container, Form, Header, SpaceBetween } from '@awsui/components-react';
+import {
+  Button,
+  Container,
+  Form,
+  Header,
+  SpaceBetween,
+} from '@awsui/components-react';
 import { PersonDetailsForm } from '@aplus/web-directory/people/ui-forms';
 import { Form as RFF } from 'react-final-form';
 
 export const CreatePersonPage = () => {
+  const submitPerson = (formValues: unknown) => {
+    console.log('submit', formValues);
+  };
+
   return (
-    <RFF onSubmit={() => {}}>
+    <RFF onSubmit={submitPerson}>
       {({ handleSubmit, values }) => (
         <form onSubmit={handleSubmit}>
           <pre>{JSON.stringify(values, null, 2)}</pre>
 
-          <Form header={<Header variant={'h1'}>Create Person</Header>}>
+          <Form
+            header={<Header variant={'h1'}>Create Person</Header>}
+            actions={
+              <SpaceBetween size={'s'} direction={'horizontal'}>
+                <Button>Cancel</Button>
+
+                <Button formAction={'submit'} variant={'primary'}>
+                  Create
+                </Button>
+              </SpaceBetween>
+            }
+          >
             <SpaceBetween size={'l'}>
               <Container header={<Header>Details</Header>}>
                 <PersonDetailsForm />
