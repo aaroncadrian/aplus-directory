00cf8827120384e883056d06615a25efcc2bac63
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-29 15:00:28 -0700

    Register client

--- apps/web-directory/src/environments/environment.prod.ts
+++ apps/web-directory/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
-export const environment = {
+import { WebDirectoryEnvironment } from './web-directory-environment.interface';
+
+export const environment: WebDirectoryEnvironment = {
   production: true,
+
+  apollo: {
+    // TODO: Pass production variable for uri
+    uri: '',
+  },
 };
--- apps/web-directory/src/environments/environment.ts
+++ apps/web-directory/src/environments/environment.ts
@@ -1,6 +1,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
 
-export const environment = {
+import { WebDirectoryEnvironment } from './web-directory-environment.interface';
+
+export const environment: WebDirectoryEnvironment = {
   production: false,
+
+  apollo: {
+    uri: 'http://localhost:3333/graphql',
+  },
 };
