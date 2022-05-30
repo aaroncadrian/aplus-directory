1c6665d19ea9cff0b1905c551b16e30d3ad3a88a
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-30 11:47:14 -0700

    Use GQL mutation

--- libs/web-directory/people/feature-create/src/lib/create-person-page.tsx
+++ libs/web-directory/people/feature-create/src/lib/create-person-page.tsx
@@ -15,18 +15,39 @@ import {
 import { Form as RFF } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { PrefixProvider } from '@aplus/react/ui-forms';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCreatePerson } from './use-create-person';
+import { CreatePersonFormSchema } from './create-person-form.schema';
 
 export const CreatePersonPage = () => {
-  const submitPerson = (formValues: unknown) => {
-    console.log('submit', formValues);
-  };
-
   const navigate = useNavigate();
 
+  const { execute: createPerson, loading, data, error } = useCreatePerson();
+
+  useEffect(() => {
+    if (!loading && data) {
+      navigate(`../${data.id}`);
+    }
+  }, [loading, data, navigate]);
+
+  const submitPerson = (formValues: CreatePersonFormSchema) => {
+    console.log('submit', formValues);
+
+    createPerson({
+      input: {
+        emailAddress: formValues.emailAddress,
+        firstName: formValues.firstName,
+        middleName: formValues.middleName,
+        lastName: formValues.lastName,
+        phones: formValues.phones,
+        postalAddress: formValues.postalAddress,
+      },
+    });
+  };
+
   return (
-    <RFF
+    <RFF<CreatePersonFormSchema>
       onSubmit={submitPerson}
       mutators={{
         ...arrayMutators,
@@ -35,6 +56,12 @@ export const CreatePersonPage = () => {
     >
       {({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
+          {!loading && error && (
+            <div>
+              Error: <pre>{JSON.stringify(error, null, 2)}</pre>
+            </div>
+          )}
+
           <BreadcrumbGroup
             items={[
               {
