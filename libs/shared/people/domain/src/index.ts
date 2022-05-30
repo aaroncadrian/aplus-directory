3d11414ac82ee935811f411191df97ad7458b163
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-30 14:44:58 -0700

    Add phone and postal address

--- libs/shared/people/domain/src/index.ts
+++ libs/shared/people/domain/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/person';
+export * from './lib/phone';
+export * from './lib/postal-address';
---
+++ libs/shared/people/domain/src/lib/postal-address.ts
@@ -0,0 +1,8 @@
+export interface PostalAddress {
+  line1: string;
+  line2?: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  country: string;
+}
