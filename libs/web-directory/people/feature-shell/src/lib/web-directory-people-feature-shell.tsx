367ac70478ac7b97e4b704c2aade1db744661948
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-30 14:34:04 -0700

    Register describe route

--- libs/web-directory/people/feature-shell/src/lib/web-directory-people-feature-shell.tsx
+++ libs/web-directory/people/feature-shell/src/lib/web-directory-people-feature-shell.tsx
@@ -6,12 +6,18 @@ const CreatePersonPage = React.lazy(
   () => import('@aplus/web-directory/people/feature-create')
 );
 
+const DescribePersonPage = React.lazy(
+  () => import('@aplus/web-directory/people/feature-describe')
+);
+
 export function WebDirectoryPeopleFeatureShell() {
   return (
     <Routes>
       <Route index element={<ListPeoplePage />} />
 
       <Route path={'create'} element={<CreatePersonPage />} />
+
+      <Route path={':personId'} element={<DescribePersonPage />} />
     </Routes>
   );
 }
